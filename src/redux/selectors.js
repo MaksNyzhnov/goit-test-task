c551de99f386209237d98e40ec374546ad7686ce
MaksNyzhnov <niznovmaksim@gmail.com>
2023-12-11 22:20:56 +0200

    fixed model and deleted assets folder

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,5 +1,5 @@
 export const getAdverts = state => state.items;
-export const getCurrentAdvert = state => state.cars.currentItem;
+export const getCurrentAdvert = state => state.currentItem;
 export const getIsModalOpen = state => state.isModalOpen;
 export const getFavorites = state => state.favorites;
 export const getFilter = state => state.filter;
